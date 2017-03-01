ea9330a32b03a274df35e5fb84077b991511d629
yl1003 <yulei1943@gmail.com>
2017-03-01 22:34:14 +0800

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-
 import { AuthService } from './core/auth.service';
+import { routing } from './app.routes';
+import { TodoComponent } from './todo/todo.component';
+import { InMemoryTodoDbService } from './todo/todo-data';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryTodoDbService),
+    routing
   ],
   providers: [
     { provide: 'auth', useClass: AuthService }
---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from './todo.model'
+import { TodoService } from './todo.service';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css'],
+  providers: [TodoService]
+})
+export class TodoComponent implements OnInit {
+  todos: Todo[] = [];
+  desc = '';
+  constructor(private service:TodoService) { }
+
+  ngOnInit() {
+  }
+
+  addTodo() {
+    this.service
+      .addTodo(this.desc)
+      .then(todo => {
+        this.todos = [...this.todos, todo];
+        this.desc = '';
+      })
+  }
+}
